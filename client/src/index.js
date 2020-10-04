fa6e887f3070a82fe16d5ad4b24b4ed5111ab3ac
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-03 23:40:04 -0600

    updated index.js with auth0 and added auth files

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
+import './index.css';
+import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import config from "./auth_config.json";
+import history from "./utils/history";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.returnTo
+      ? appState.returnTo
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Router>
+    <Auth0Provider
+      domain={config.domain}
+      clientId={config.clientId}
+      audience={config.audience}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
+  </Router>,
+  document.getElementById('root')
+);
