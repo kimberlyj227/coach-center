e19936a8d34921053df97c74672fb15ad0e5f811
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-03 22:58:49 -0600

    updating file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+
+     <Switch>
+        <Route exact path={["/", "/books"]} component={Books} />
+        <Route exact path = "/books/:id" component={Detail} />
+       <Route>
+         <NoMatch day="Friday"/>
+       </Route>
+     </Switch>
+
+    </Router>
   );
 }
 
